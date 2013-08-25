afda9fd6a4e31ded2e551a2901866d561d7c6be5
Don Morrison <don@elskwid.net>
2013-08-25 15:06:38 -0700

    Add modes.el to map modes to file extensions

---
+++ elskwid/settings/modes.el
@@ -0,0 +1,32 @@
+;; css-mode
+(add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
+
+;; markdown-mode
+(add-to-list 'auto-mode-alist '("\\.markdn\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'"  . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown\\'"  . markdown-mode))
+
+;; ruby-mode
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
+
+;; scss-mode
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+
+;; web-mode
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
